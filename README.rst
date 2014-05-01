66e2a81998da9b67810900ce488caf3820df18d3
Cychih <michael66230@gmail.com>
2014-05-01 20:40:39 +0800

    Add ifconfig support, add python3 support

--- README.rst
+++ README.rst
@@ -7,3 +7,12 @@ so I wrote a little Python script to parse the result of ``ipconfig`` to get the
 The encoding of ``ipconfig`` is ``CP950``, I decoded it and encode it again with ``UTF-8``.
 
 The function in the script returns a dictionary, with key as name and value as IP address.
+
+20140501 update
+---------------
+
+Add ``ifconfig`` support to this program.
+
+When tried upgrade to Python3 (still Python2 compatiable),
+I found ``subprocess`` module in Python3 has some problem in UTF-8 environment,
+so this program is not support in Cygwin+Python3.
--- show_my_ip.py
+++ show_my_ip.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import re
 import subprocess as sub
+import os
+import sys
 
-def show_my_ip ():
-    ipconfig = sub.check_output(['ipconfig'])
+VERSION = sys.version_info[0]
+RUN_IN_WINDOWS = os.uname()[0].startswith('CYGWIN_NT')
+
+def show_my_ip_ipconfig ():
+    ipconfig = str(sub.check_output(['ipconfig']))
     ipconfig = ipconfig.decode('cp950').encode('utf8').replace('\r\n','\n')
     patt = r'''\n
                (?P<NAME>\S.*?):\n
@@ -13,9 +19,33 @@ def show_my_ip ():
             '''
     return dict(m.groups() for m in re.finditer(patt, ipconfig, re.VERBOSE))
 
+def show_my_ip_ifconfig ():
+    if VERSION == 3:
+        ifconfig = str(sub.getoutput(['ifconfig']))#.decode('utf-8')
+    else:
+        ifconfig = str(sub.check_output(['ifconfig']))#.decode('utf-8')
+    patt = r'''(?P<NAME>\w+):\ flags.*\n
+               (?:\s+.*?\n)*
+               \s+inet\ (?P<IP>.*)\ netmask.*
+            '''
+    return dict(m.groups() for m in re.finditer(patt, ifconfig, re.VERBOSE))
+
+def show_my_ip ():
+    if RUN_IN_WINDOWS:
+        return show_my_ip_ipconfig()
+    return show_my_ip_ifconfig()
+
 if __name__ == '__main__':
-    print 'Detected IP addresses:\n'
-    for i,j in show_my_ip().iteritems():
-        print i + ':'
-        print '   ',j
-        print ''
+
+    print('Python version %d detected.'%(VERSION))
+    if VERSION == 3 and RUN_IN_WINDOWS:
+        print('This program is not supported in Python3 and in Cygwin, use Python2 insteed.')
+        print('Sorry for the inconvenience.')
+        exit()
+
+    print('Detected IP addresses:\n')
+    result = show_my_ip()
+    for i in result:
+        print(i + ':')
+        print('   ', result[i])
+        print('')
